d1c307da85ba6f9aae22819d8b00657c477aa971
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-13 15:32:45 -0400

    Commented out list items in index.html to start implementing localstorage. Started adding localstorage to app.js, added addItem, addListItem, initialState, getLists, getListsItems, parseList functions to abstract.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
   if (!storageStatus('localStorage'))
     $('.storageErrorStatus')[0].style.visibility = 'visible';
+  initialState();
+  var shopLists = getLists();
+  var listItems = getListsItems(shopLists[0]);
+  parseList(listItems, addItem);
+  //localStorage.removeItem('List 1');
+
   $(document).on('mousedown', '.delete', (function(){
     if ($(this).prevAll('input').prop('checked') && $(this).prevAll('input').is(':radio') &
     $(this).parent().prev().find('input').length){
@@ -12,16 +18,10 @@ $(document).ready(function () {
     $(this).parent().remove();
   }));
   $('#listNameButton').click(function() {
-    $('.listNames ul').append('<li><input type="radio" name="listName" ' +
-    'checked="true" value="list1"><label for=""> ' + $('#listName').val() + '</label><i class="fa ' +
-    'fa-times delete" aria-hidden="true"></i></li>');
-    $('#listName').val('');
+    addListItem($('#listName').val());
   });
   $('#itemNameButton').click(function() {
-    $('.listItems ul').append('<li><input type="checkbox" class="checkList">' +
-    '<label for="" class="checkListName"> ' + $('#itemName').val() +'</label><i class="fa fa-times  ' +
-    'delete" aria-hidden="true"></i></li>');
-    $('#itemName').val('');
+    addItem($('#itemName').val());
   });
 });
 function storageStatus(type) {
@@ -34,4 +34,37 @@ function storageStatus(type) {
 	catch(e) {
 		return false;
 	}
+}
+function addItem(name) {
+  $('.listItems ul').append('<li><input type="checkbox" class="checkList">' +
+  '<label for="" class="checkListName"> ' + name +'</label><i class="fa fa-times  ' +
+  'delete" aria-hidden="true"></i></li>');
+  $('#itemName').val('');
+}
+function addListItem(name) {
+  $('.listNames ul').append('<li><input type="radio" name="listName" ' +
+  'checked="true" value="list1"><label for=""> ' + name + '</label><i class="fa ' +
+  'fa-times delete" aria-hidden="true"></i></li>');
+  $('#listName').val('');
+}
+function initialState() {
+  if (localStorage.getItem('Shopping Lists') === null) {
+    var value = ['List 1'];
+    localStorage.setItem('Shopping Lists', JSON.stringify(value));
+    if (localStorage.getItem("List 1") === null) {
+      value = ['Milk', 'Pepper', 'Salt', 'Eggs', 'Dish Detergent'];
+      localStorage.setItem('List 1', JSON.stringify(value));
+    }
+  }
+}
+function getLists() {
+  return JSON.parse(localStorage.getItem('Shopping Lists'));
+}
+function getListsItems(list) {
+  return JSON.parse(localStorage.getItem(list));
+}
+function parseList(list, methodCallBack){
+  for (var x in list){
+    methodCallBack(list[x]);
+  }
 }
