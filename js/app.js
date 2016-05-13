efb4cd08172791ffe0341614b919858d8a5043c4
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-13 02:40:48 -0400

    Changed mousedown event handler to check the next radio button if the button was checked and only a radio button, added append for listnameform in app.js.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+var count = 5;
 $(document).ready(function () {
-  $('.delete').mousedown(function(){
+  $(document).on('mousedown', '.delete', (function(){
+    if ($(this).prevAll('input').prop('checked') && $(this).prevAll('input').is(':radio'))
+      $(this).parent().next().find('input').prop('checked', true);
     $(this).parent().remove();
+    //alert('here');
+  }));
+  $('#listNameForm').click(function() {
+    console.log($(this).next());
+    $('.listNames ul').append('<li><input type="radio" name="listName" ' +
+    'checked="true" value="list1"><label for="">List ' + count + '</label><i class="fa ' +
+    'fa-times delete" aria-hidden="true"></i></li>');
+    count++;
   });
 });
