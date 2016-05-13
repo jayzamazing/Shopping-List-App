02df602dd16a91180dace0ce14c35f6b0168f98d
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-13 11:45:43 -0400

    Changed a few classnames/ids for the forms, inputs, and buttons in index.html. Added function for listItemForm to add elements, changed click events to trigger of buttons instead of form in app.js.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-var count = 5;
 $(document).ready(function () {
   $(document).on('mousedown', '.delete', (function(){
-    if ($(this).prevAll('input').prop('checked') && $(this).prevAll('input').is(':radio'))
+    console.log($(this).prevAll('input').prop('checked'));
+    console.log($(this).parent().prev().find('input').length);
+    console.log($(this).parent().next().find('input').length);
+    if ($(this).prevAll('input').prop('checked') && $(this).prevAll('input').is(':radio') &
+    $(this).parent().prev().find('input').length){
+      $(this).parent().prev().find('input').prop('checked', true);
+    } else if ($(this).prevAll('input').prop('checked') && $(this).prevAll('input').is(':radio') &
+    $(this).parent().next().find('input').length) {
       $(this).parent().next().find('input').prop('checked', true);
+    }
     $(this).parent().remove();
-    //alert('here');
   }));
-  $('#listNameForm').click(function() {
-    console.log($(this).next());
+  $('#listNameButton').click(function() {
     $('.listNames ul').append('<li><input type="radio" name="listName" ' +
-    'checked="true" value="list1"><label for=""> List ' + count + '</label><i class="fa ' +
+    'checked="true" value="list1"><label for=""> ' + $('#listName').val() + '</label><i class="fa ' +
     'fa-times delete" aria-hidden="true"></i></li>');
-    count++;
+    $('#listName').val('');
+  });
+  $('#itemNameButton').click(function() {
+    $('.listItems ul').append('<li><input type="checkbox" class="checkList">' +
+    '<label for="" class="checkListName"> ' + $(itemName).val() +'</label><i class="fa fa-times  ' +
+    'delete" aria-hidden="true"></i></li>');
+    $(itemName).val('');
   });
 });
